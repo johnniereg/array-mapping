3b484d63c69f5ff53ba48aff5d0b1dcd4bcf6aa0
Johnnie Regalado <johnniereg@gmail.com>
2017-11-02 18:43:44 +0000

    Problem solved, pythagorean theorem in action.

--- array-mapping.js
+++ array-mapping.js
@@ -3,7 +3,12 @@ var input = [
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+
+var result = input.map(function(numbers) {
+  return Math.sqrt(Math.pow(numbers.x, 2) + Math.pow(numbers.y, 2));
+});
+
+console.log(result);
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
